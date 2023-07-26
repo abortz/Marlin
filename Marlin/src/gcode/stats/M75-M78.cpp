39672014115b8692ff879983206b2d237c539e88
Andrew Bortz <andrew@abortz.net>
2023-07-26 20:35:48 +0000

    Ender 3 S1 config + non-linear extrusion

--- Marlin/src/gcode/stats/M75-M78.cpp
+++ Marlin/src/gcode/stats/M75-M78.cpp
@@ -47,7 +47,7 @@ void GcodeSuite::M75() {
  * M76: Pause print timer
  */
 void GcodeSuite::M76() {
-  TERN(DWIN_LCD_PROUI, ui.pause_print(), print_job_timer.pause());
+  print_job_timer.pause();
   TERN_(HOST_PAUSE_M76, hostui.pause());
 }
 
